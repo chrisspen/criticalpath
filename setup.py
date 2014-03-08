e59a949aba2d249bda4f1a364e6beb80b1bdf2a1
chris <chris@coronis>
2014-03-07 23:57:29 -0500

    Updated classifiers.

--- setup.py
+++ setup.py
@@ -15,9 +15,10 @@ setup(
     description = "Calculates the critical path in a task network.",
     license = "LGPL",
     url = "https://github.com/chrisspen/criticalpath",
+    #https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers = [
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: LGPL License',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
     ],
